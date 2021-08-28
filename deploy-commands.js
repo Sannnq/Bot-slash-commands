c29fde4ffa2ab0e2235f2b636cf073b5692987aa
먼저 (San) <66654707+Sannnq@users.noreply.github.com>
2021-08-28 14:34:15 +0200

    Add files via upload

---
+++ deploy-commands.js
@@ -0,0 +1,14 @@
+const fs=require('fs');
+const {REST}= require('@discordjs/rest');
+const{Routes}= require('discord-api-types/v9');
+const { CLIENT_ID,GUILD_ID,token } = require('./config.json');
+
+const commands = [];
+const commandFiles=fs.readdirSync('./commands').filter(file=>file.endsWith('.js'));
+
+
+for(const file of commandFiles){
+    const command=require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+console.log('Succesfull Loaded!');
